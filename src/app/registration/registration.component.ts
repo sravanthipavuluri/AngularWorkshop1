877bbf3bc240053f5cd243e864a58165f6789bdc
sravanthipavuluri <sravanthipavuluri20@gmail.com>
2018-03-10 13:09:22 +0530

    commit

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -21,11 +21,10 @@ export class RegistrationComponent {
     this.registrationForm = this.fb.group({
       firstName:['', Validators.required],
       lastName:['',Validators.required],
-      email:['',Validators.compose([Validators.required ,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])],
+      email:['',Validators.compose([Validators.required ,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])],                                                                             
       mobile:['',Validators.compose([Validators.required,Validators.pattern('\\d{9}')])],
-      wrkstatus:['',Validators.required],
-      techInterests:['',Validators.required],
-      btnSubmit:['']
+      wrkstatus:['Work Status',Validators.required],
+      techInterests:['',Validators.required]
       // address: this.fb.group({ // <-- the child FormGroup
       //   street: ['', Validators.required ],
       //   city: ['', Validators.required ],
